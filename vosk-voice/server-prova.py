9d3377456a09f77d67dfd622ae544e8bac7d08f5
klairt <klair.tulip.studente@itispaleocapa.it>
2026-03-05 15:20:54 +0100

    vosk voice

---
+++ vosk-voice/server-prova.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6f5c9da4616645859d65698c9b344c134671f23acf8286bdb2322f94434d606f
+size 5154
---
+++ vosk-voice/server.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7b0e715e8db49437e35e2a0c4d4a2ec1492cfb16c053143dc59ad450d384b8d
+size 1849
